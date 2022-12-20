72eddd1ef6e680eee652578b3bee02cc90fea550
basicactor <careergo357@gmail.com>
2022-12-20 12:46:26 +0900

    ファイルインポート時に@マークを使えるようにする

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "../views/HomeView.vue"
-import Form from "../views/forms/FormMain.vue"
+import HomeView from "@/views/HomeView.vue"
+import Form from "@/views/forms/FormMain.vue"
 
 const routes = [
   { path: "/", component: HomeView },
